fcc77bc00e0eaf25b42cf749f5ac5f655b395b35
Guruprasad M <guruprasad.m195@gmail.com>
2026-09-08 10:19:04 +0000

    Add guide1 client script customization

---
+++ guide1/hooks.py
@@ -0,0 +1,343 @@
+app_name = "app1"
+app_title = "App1"
+app_publisher = "Guruprasad"
+app_description = " My first Frappe app"
+app_email = "guruprasad.m195@gmail.com"
+app_license = "mit"
+
+
+# website_clear_cache = "app1.overrides.clear_website_cache"
+
+
+# website_redirects = [
+#     {
+#         "source": "/about-us",
+#         "target": "/about"
+#     },
+    
+# ]
+
+# website_route_rules = [
+#     {
+#         "from_route": "/projects/<name>",
+#         "to_route": "app1/projects/project"
+#     }
+# ]
+
+# website_path_resolver = "app1.website.resolve_path"
+
+# website_catch_all = "not_found"
+
+# homepage = "about"
+
+# role_home_page = {
+#     "Customer": "orders",
+#     "Supplier": "bills",
+#     "Accounts User": "dashboard"
+# }
+
+# get_website_user_home_page = "app1.website.get_home_page"
+
+# portal_menu_items = [
+#     {
+#         "title": "Orders",
+#         "route": "/orders",
+#         "role": "Customer"
+#     },
+#     {
+#         "title": "Employee",
+#         "route": "/employee",
+#         "role": "Customer"
+#     }
+# ]
+
+# brand_html = """
+# <a href="/" style="display:flex;align-items:center;gap:10px;text-decoration:none;">
+#    <img src="/assets/app1/images/Bugatti_logo.png" height="35">
+#     <span style="font-size:20px;font-weight:bold;">
+#         Pur Sport
+#     </span>
+# </a>
+# """
+
+# Apps
+# ------------------
+
+# required_apps = []
+
+# Each item in the list will be shown as an app in the apps page
+# add_to_apps_screen = [
+# 	{
+# 		"name": "app1",
+# 		"logo": "/assets/app1/logo.png",
+# 		"title": "App1",
+# 		"route": "/app1",
+# 		"has_permission": "app1.api.permission.has_app_permission"
+# 	}
+# ]
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/app1/css/app1.css"
+# app_include_js = "/assets/app1/js/app1.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/app1/css/app1.css"
+# web_include_js = "/assets/app1/js/app1.js"
+
+# include custom scss in every website theme (without file extension ".scss")
+# website_theme_scss = "app1/public/scss/website"
+
+# include js, css files in header of web form
+# webform_include_js = {"doctype": "public/js/doctype.js"}
+# webform_include_css = {"doctype": "public/css/doctype.css"}
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "app1/public/icons.svg"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+# 	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# automatically load and sync documents of this doctype from downstream apps
+# importable_doctypes = [doctype_1]
+
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+# 	"methods": "app1.utils.jinja_methods",
+# 	"filters": "app1.utils.jinja_filters"
+# }
+
+# Installation
+# ------------
+
+# before_install = "app1.install.before_install"
+# after_install = "app1.install.after_install"
+
+# Uninstallation
+# ------------
+
+# before_uninstall = "app1.uninstall.before_uninstall"
+# after_uninstall = "app1.uninstall.after_uninstall"
+
+# Integration Setup
+# ------------------
+# To set up dependencies/integrations with other apps
+# Name of the app being installed is passed as an argument
+
+# before_app_install = "app1.utils.before_app_install"
+# after_app_install = "app1.utils.after_app_install"
+
+# Integration Cleanup
+# -------------------
+# To clean up dependencies/integrations with other apps
+# Name of the app being uninstalled is passed as an argument
+
+# before_app_uninstall = "app1.utils.before_app_uninstall"
+# after_app_uninstall = "app1.utils.after_app_uninstall"
+
+# Build
+# ------------------
+# To hook into the build process
+
+# after_build = "app1.build.after_build"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "app1.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"app1.tasks.all"
+# 	],
+# 	"daily": [
+# 		"app1.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"app1.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"app1.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"app1.tasks.monthly"
+# 	],
+# }
+
+# Testing
+# -------
+
+# before_tests = "app1.install.before_tests"
+
+# Extend DocType Class
+# ------------------------------
+#
+# Specify custom mixins to extend the standard doctype controller.
+# extend_doctype_class = {
+# 	"Task": "app1.custom.task.CustomTaskMixin"
+# }
+
+# Overriding Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "app1.event.get_events"
+# }
+#
+# each overriding function accepts a `data` argument;
+# generated from the base implementation of the doctype dashboard,
+# along with any modifications made in other Frappe apps
+# override_doctype_dashboards = {
+# 	"Task": "app1.task.get_dashboard_data"
+# }
+
+# exempt linked doctypes from being automatically cancelled
+#
+# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+
+# Ignore links to specified DocTypes when deleting documents
+# -----------------------------------------------------------
+
+# ignore_links_on_delete = ["Communication", "ToDo"]
+
+# Request Events
+# ----------------
+# before_request = ["app1.utils.before_request"]
+# after_request = ["app1.utils.after_request"]
+
+# Job Events
+# ----------
+# before_job = ["app1.utils.before_job"]
+# after_job = ["app1.utils.after_job"]
+
+# User Data Protection
+# --------------------
+
+# user_data_fields = [
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
+# ]
+
+# Authentication and authorization
+# --------------------------------
+
+# auth_hooks = [
+# 	"app1.auth.validate"
+# ]
+
+# Automatically update python controller files with type annotations for this app.
+export_python_type_annotations = False
+
+# default_log_clearing_doctypes = {
+# 	"Logging DocType Name": 30  # days to retain logs
+# }
+
+# Translation
+# ------------
+# List of apps whose translatable strings should be excluded from this app's translations.
+# ignore_translatable_strings_from = []
+
+app_include_js = "custom_desk.bundle.js"
+
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+doc_events = {
+    "Test6": {
+        "validate": "app1.api.custom_logic"
+    }
+}
+
+scheduler_events = {
+    "daily": [
+        "app1.tasks.daily_maintenance"
+    ]
+}
+
+
+# fixtures = ["Client Script"]
+
+fixtures = [
+    {
+        "dt": "Client Script",
+        "filters": [
+            ["name", "=", "usercs"]
+        ]
+    }
+]
